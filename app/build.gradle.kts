6bb0068de364a9fea2a5b90270ec3a6cce92d5ba
Cassius <141352987+Infinifar@users.noreply.github.com>
2026-09-02 03:05:42 +0800

    Fix duplicate printReleaseVersionName task definition

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -289,14 +289,3 @@ val printReleaseVersionName by tasks.registering {
         println(AppInfo.versionName)
     }
 }
-
-
-/**
- * Prints the current version name. Used for GitHub releases.
- */
-@Suppress("unused")
-val printReleaseVersionName by tasks.registering {
-    doLast {
-        println(AppInfo.versionName)
-    }
-}
